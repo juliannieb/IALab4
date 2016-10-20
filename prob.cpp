20b6630b537fe3940f6c60b8fc5fef3128527f56
Julian NG <juliannieb@gmail.com>
2016-10-20 00:18:45 -0500

    Add node structure

---
+++ prob.cpp
@@ -0,0 +1,21 @@
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+struct node {
+	string key;
+	vector<*node> parents;
+	map<*node, int> idxOfParents;
+	map<int, double> probabilitiesTable;
+};
+
+
+
+int main () {
+	
+	return 0;
+}
