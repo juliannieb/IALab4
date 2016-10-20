0773b9353a50221f0fe73a4305b0db2a06d789af
Julian NG <juliannieb@gmail.com>
2016-10-20 00:48:59 -0500

    Function to read nodes

--- prob.cpp
+++ prob.cpp
@@ -8,14 +8,36 @@ using namespace std;
 
 struct node {
 	string key;
-	vector<*node> parents;
-	map<*node, int> idxOfParents;
+	vector<node*> parents;
+	map<node*, int> idxOfParents;
 	map<int, double> probabilitiesTable;
 };
 
+typedef struct node Node;
 
+vector<Node*> readNodes() {
+	string node_key;
+	while (cin >> node_key) {
+		if (node_key[node_key.size()-1] == ',') {
+			node_key = node_key.substr(0, node_key.size()-1);
+		}
+		cout << node_key << endl;
+	}
+}
 
 int main () {
-	
+	string input_type;
+	cin >> input_type;
+	if (input_type == "[Nodes]") {
+		readNodes();
+	}
+	cin >> input_type;
+	if (input_type == "[Probabilities]") {
+
+	}
+	cin >> input_type;
+	if (input_type == "[Queries]") {
+
+	}
 	return 0;
 }
